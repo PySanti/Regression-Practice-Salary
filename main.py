9e5d2c4e93ee3cd6d4530b4dcbebb673c415c2ec
santiago <santiagodeandrade4@gmail.com>
2025-03-06 08:07:59 -0400

    Modificacion de readme para estudio final de rendimiento de regresion lineal

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 from utils.filename_gen import filename_gen
 from utils.basic_preprocess import basic_preprocess
 import pandas as pd
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.svm import SVR
-import joblib
-from sklearn.ensemble import RandomForestRegressor
-import numpy as np
-
-
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
 
 TARGET = "Salary"
-param_grid = {
-    'n_estimators': np.arange(50, 401, 50),         # Número de árboles en el bosque
-    'max_depth': [None, 10, 20, 30, 40],         # Máxima profundidad de los árboles
-    'min_samples_split': [2, 5, 10],             # Número mínimo de muestras requeridas para dividir un nodo
-    'min_samples_leaf': [1, 2, 4],               # Número mínimo de muestras requeridas en una hoja
-    'max_features': ['sqrt', 'log2'],    # Número de características a considerar al buscar la mejor división
-    'bootstrap': [True, False]                   # Si utilizar o no bootstrap samples al construir árboles
-}
+param_list =[
+    {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'positive': False},
+    {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'positive': False},
+    {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'positive': True},
+    {'copy_X': True, 'fit_intercept': True, 'n_jobs': None, 'positive': False}
+]
 
 
+i = 0
 for scaler, pca in [(True, True), (True, False), (False, True), (False, False)]:
+
     filename = filename_gen("rf", scaler, pca)
     print(filename)
+
     [df_train, df_test, df_val] = basic_preprocess(pd.read_csv("./data/data.csv"), TARGET, scaler=scaler, pca=pca)
 
-    grid_search = RandomizedSearchCV(RandomForestRegressor(), param_distributions=param_grid, cv=4, verbose=10,n_jobs=6, scoring="neg_mean_absolute_error", n_iter=300)
-    grid_search.fit(df_train.drop(TARGET, axis=1), df_train[TARGET])
+    best_params = param_list[i]
+    alg = LinearRegression(**best_params)
+    alg.fit(df_train.drop(TARGET, axis=1), df_train[TARGET])
+
+    # train
+    y_pred = alg.predict(df_train.drop(TARGET, axis=1))
+    print(f"MAE para train : {mean_absolute_error(df_train[TARGET], y_pred):.3f}")
+    print(f"R2 para train : {r2_score(df_train[TARGET], y_pred):.3f}")
+
+    # test
+    y_pred = alg.predict(df_test.drop(TARGET, axis=1))
+    print(f"MAE para test : {mean_absolute_error(df_test[TARGET], y_pred):.3f}")
+    print(f"R2 para test : {r2_score(df_test[TARGET], y_pred):.3f}")
 
 
-    print("Mejores hiperparametros")
-    print(grid_search.best_estimator_.get_params())
-    print("Mejor MAE")
-    print(grid_search.best_score_*-1)
-
-    joblib.dump(grid_search.best_estimator_, filename)
-
-
+    i+=1
